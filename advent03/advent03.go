ca0f82eb45892966a1802e34e014f90a104b06e0
James Burns <james.burns@rtslabs.com>
2025-12-03 18:21:42 -0500

    day 3 part 1

--- advent03/advent03.go
+++ advent03/advent03.go
@@ -1,6 +1,40 @@
 package advent03
 
+import (
+	"strconv"
+
+	"advent2024/util"
+)
+
 func Solution(inputFile string) (part1, part2 any) {
-	//lines := util.ReadFile(inputFile)
-	return 0, 0
+	lines := util.ReadFile(inputFile)
+
+	part1Jotalge := 0
+	for _, line := range lines {
+		currentMax := 0
+		maxFirstChar := '0'
+		for i, char := range line {
+			if i >= len(line)-1 {
+				break
+			}
+			if char < maxFirstChar {
+				continue
+			}
+			maxFirstChar = char
+			maxSecondChar := '0'
+			for _, char2 := range line[i+1:] {
+				if char2 < maxSecondChar {
+					continue
+				}
+				maxSecondChar = char2
+			}
+			v, _ := strconv.Atoi(string(maxFirstChar) + string(maxSecondChar))
+			if v > currentMax {
+				currentMax = v
+			}
+		}
+		part1Jotalge += currentMax
+	}
+
+	return part1Jotalge, 0
 }
