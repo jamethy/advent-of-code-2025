3ed91f3355f8b6e2205ed35aa925d95376e2c2d2
James Burns <james.burns@rtslabs.com>
2025-12-09 18:39:06 -0500

    day 9 part 2

--- advent09/advent09.go
+++ advent09/advent09.go
@@ -1,6 +1,10 @@
 package advent09
 
 import (
+	"cmp"
+	"math"
+	"slices"
+
 	"advent2024/util"
 	"advent2024/util/mathutil"
 )
@@ -17,16 +21,232 @@ func Solution(inputFile string) (part1, part2 any) {
 		points[i] = Point{nums[0], nums[1]}
 	}
 
-	biggestArea := 0
+	part1BiggestArea := 0
 	for i, a := range points {
 		for j := i + 1; j < len(points); j++ {
 			b := points[j]
 			area := (mathutil.AbsInt(a.x-b.x) + 1) * (mathutil.AbsInt(a.y-b.y) + 1)
-			if area > biggestArea {
-				biggestArea = area
+			if area > part1BiggestArea {
+				part1BiggestArea = area
 			}
 		}
 	}
 
-	return biggestArea, 0
+	//part2BiggestArea := 0
+	//for i, a := range points {
+	//	for j := i + 1; j < len(points); j++ {
+	//		b := points[j]
+	//		area := (mathutil.AbsInt(a.x-b.x) + 1) * (mathutil.AbsInt(a.y-b.y) + 1)
+	//		if area < part2BiggestArea {
+	//			continue
+	//		}
+	//
+	//		noPointsInside := true
+	//		for _, c := range points {
+	//			if isInside(a, b, c) {
+	//				noPointsInside = false
+	//				break
+	//			}
+	//		}
+	//
+	//		if noPointsInside {
+	//			part2BiggestArea = area
+	//		}
+	//	}
+	//}
+
+	horizontalLines := make([][]Point, 0, len(points))
+	verticalLines := make([][]Point, 0, len(points))
+	for li, pi := range points {
+		lj := li + 1
+		if lj >= len(points) {
+			lj = 0
+		}
+		pj := points[lj]
+		if pj.x == pi.x {
+			verticalLines = append(verticalLines, []Point{pi, pj})
+		} else {
+			horizontalLines = append(horizontalLines, []Point{pi, pj})
+		}
+	}
+	slices.SortFunc(verticalLines, func(a, b []Point) int {
+		return cmp.Compare(a[0].x, b[0].x)
+	})
+	slices.SortFunc(horizontalLines, func(a, b []Point) int {
+		return cmp.Compare(a[0].y, b[0].y)
+	})
+
+	//a.x == 9 && a.y == 5 && b.x == 2 && b.y == 3
+	part2BiggestArea := 0 // 9,5 and 2,3
+	for i, a := range points {
+		for j := i + 1; j < len(points); j++ {
+			b := points[j]
+			area := (mathutil.AbsInt(a.x-b.x) + 1) * (mathutil.AbsInt(a.y-b.y) + 1)
+			if area < part2BiggestArea {
+				continue
+			}
+
+			left := mathutil.MinInt(a.x, b.x)
+			right := mathutil.MaxInt(a.x, b.x)
+			top := mathutil.MinInt(a.y, b.y)
+			bottom := mathutil.MaxInt(a.y, b.y)
+
+			pointsInside := false
+			for _, c := range points {
+				if c.x <= left {
+					continue
+				}
+				if c.x >= right {
+					continue
+				}
+				if c.y <= top {
+					continue
+				}
+				if c.y >= bottom {
+					continue
+				}
+				pointsInside = true
+				break
+			}
+			if pointsInside {
+				continue
+			}
+
+			cutByLine := false
+			for _, hl := range horizontalLines {
+				if hl[0].y <= top || hl[0].y >= bottom {
+					continue
+				}
+				lineLeft := mathutil.MinInt(hl[0].x, hl[1].x)
+				if lineLeft > left {
+					continue
+				}
+				lineRight := mathutil.MaxInt(hl[0].x, hl[1].x)
+				if lineRight < right {
+					continue
+				}
+				cutByLine = true
+				break
+			}
+			if cutByLine {
+				continue
+			}
+
+			for _, vl := range verticalLines {
+				if vl[0].x <= left || vl[0].x >= right {
+					continue
+				}
+				lineTop := mathutil.MinInt(vl[0].y, vl[1].y)
+				if lineTop < top {
+					continue
+				}
+				lineBottom := mathutil.MaxInt(vl[0].y, vl[1].y)
+				if lineBottom > bottom {
+					continue
+				}
+				cutByLine = true
+				break
+			}
+			if cutByLine {
+				continue
+			}
+
+			otherCorners := []Point{
+				{a.x, b.y},
+				{b.x, a.y},
+			}
+			otherCornersGood := true
+			for _, o := range otherCorners {
+
+				var closestLine []Point
+				closestDist := math.MaxInt
+				for _, hl := range horizontalLines {
+					dist := mathutil.AbsInt(o.y - hl[0].y)
+					if dist >= closestDist {
+						break // since they are sorted
+					}
+					if !isBetween(o.x, hl[0].x, hl[1].x) {
+						continue
+					}
+					closestLine = hl
+					closestDist = dist
+					if dist == 0 {
+						break
+					}
+				}
+
+				if closestDist == 0 {
+					continue
+				}
+				if !isRightOfLine(o, closestLine) {
+					otherCornersGood = false
+					break
+				}
+
+				closestDist = math.MaxInt
+				for _, vl := range verticalLines {
+					dist := mathutil.AbsInt(o.x - vl[0].x)
+					if dist >= closestDist {
+						break // since they are sorted
+					}
+					if !isBetween(o.y, vl[0].y, vl[1].y) {
+						continue
+					}
+					closestLine = vl
+					if dist == 0 {
+						break
+					}
+				}
+
+				if closestDist == 0 {
+					continue
+				}
+				if !isRightOfLine(o, closestLine) {
+					otherCornersGood = false
+					break
+				}
+			}
+
+			if otherCornersGood {
+				part2BiggestArea = area
+			}
+		}
+	}
+
+	return part1BiggestArea, part2BiggestArea
+}
+
+func isBetween(v, a, b int) bool {
+	if a > b {
+		a, b = b, a
+	}
+	return v >= a && v <= b
+}
+
+func isRightOfLine(a Point, line []Point) bool {
+	delta := Point{
+		x: a.x - line[0].x,
+		y: a.y - line[0].y,
+	}
+	lineRotated90 := Point{
+		x: line[0].y - line[1].y,
+		y: line[1].x - line[0].x,
+	}
+	return delta.x*lineRotated90.x+delta.y*lineRotated90.y > 0
+}
+
+func isInside(left, right, top, bottom int, c Point) bool {
+	if c.x <= left {
+		return false
+	}
+	if c.x >= right {
+		return false
+	}
+	if c.y <= top {
+		return false
+	}
+	if c.y >= bottom {
+		return false
+	}
+	return true
 }
--- advent09/advent09_test.go
+++ advent09/advent09_test.go
@@ -14,10 +14,12 @@ func TestSolution(t *testing.T) {
 		{
 			name:      "sample",
 			wantPart1: 50,
+			wantPart2: 24,
 		},
 		{
 			name:      "input",
-			wantPart1: 4738108384,
+			wantPart1: 4_738_108_384,
+			wantPart2: 1513792010,
 		},
 	}
 	for _, tt := range tests {
@@ -30,6 +32,70 @@ func TestSolution(t *testing.T) {
 		t.Run(tt.name+"-part2", func(t *testing.T) {
 			if tt.wantPart2 != nil && !reflect.DeepEqual(gotPart2, tt.wantPart2) {
 				t.Errorf("Solution() gotPart2 = %v, want %v", gotPart2, tt.wantPart2)
+			}
+		})
+	}
+}
+
+func Test_isRightOfLine(t *testing.T) {
+	type args struct {
+		a    Point
+		line []Point
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "vertical-down-right",
+			args: args{
+				a: Point{x: 8, y: 8},
+				line: []Point{
+					{x: 9, y: 3},
+					{x: 9, y: 11},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "vertical-up-right",
+			args: args{
+				a: Point{x: 10, y: 10},
+				line: []Point{
+					{x: 9, y: 11},
+					{x: 9, y: 3},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "vertical-down-left",
+			args: args{
+				a: Point{x: 10, y: 10},
+				line: []Point{
+					{x: 9, y: 3},
+					{x: 9, y: 11},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "vertical-up-left",
+			args: args{
+				a: Point{x: 8, y: 10},
+				line: []Point{
+					{x: 9, y: 11},
+					{x: 9, y: 3},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRightOfLine(tt.args.a, tt.args.line); got != tt.want {
+				t.Errorf("isRightOfLine() = %v, want %v", got, tt.want)
 			}
 		})
 	}
